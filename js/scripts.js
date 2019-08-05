400f49fb491c46423294e07e93ad7a3c2d72581e
kimbagira Sylvie <sylviekimbagira@gmail.com>
2019-08-05 13:04:44 +0200

    added prompt element in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,39 +9,17 @@ function Order(size,crust,toppings,quantity,delivery){
 Order.prototype.allInformation=function(){
   return this.sizeofpizza + "," + this.crustofpizza + "," + this.toppingsofpizza + "," + this.quantityofpizza + "," + this.deliveryofpizza;
 }
-
 //user interface Logic
 $(document).ready(function(){
 $('form#pizza').submit(function(event){
   event.preventDefault();
-    var newOrder;
-    if($('#size1').val()){
-    var inputtedsizeofpizza=$('#size1').val() + " " + "price=1000frw";
-      console.log(inputtedsizeofpizza)
-    }
-    else if($('#size2').val()){
-      var inputtedsizeofpizza=$('#size2').val() + " " + "price=7000frw" ;
-      console.log(inputtedsizeofpizza)
-    }
 
-  else if($('#size3').val()){
-      var inputtedsizeofpizza=$('#size3').val() + " " + "price=20000frw";
-      console.log(inputtedsizeofpizza)
-    }
+var inputtedsizeofpizza=$("#hide").val();
+    console.log(inputtedsizeofpizza)
+    
+    var inputtedcrustofpizza=$("#hide1").val();
+    console.log(inputtedcrustofpizza)
 
-    if($('#crust1').val()){
-      var inputtedcrustofpizza=$('#crust1').val()  + " " + "price=7000frw";
-        console.log(inputtedcrustofpizza)
-      }
-    else if($('#crust2').val()){
-      var inputtedcrustofpizza=$('#crust2').val() + " " + "price=8000frw";
-      console.log(inputtedcrustofpizza)
-    }
-
-  else if($('#crust3').val()){
-      var inputtedcrustofpizza=$('#crust3').val() + " " + "price=9000frw";
-      console.log(inputtedcrustofpizza)
-    }
   var thechoosentoppings=[];
   $('input[type=checkbox]:checked').each(function () {
     thechoosentoppings.push($(this).val());
@@ -49,7 +27,6 @@ $('form#pizza').submit(function(event){
 
   console.log(thechoosentoppings);
   var additiontoppings = 0;
-  function toppings(thechoosentoppings){
 for(var a=0; a<thechoosentoppings.length; a++){
   if(thechoosentoppings[a]==="pepperont"){
     additiontoppings += 500
@@ -61,7 +38,7 @@ for(var a=0; a<thechoosentoppings.length; a++){
  }
  else if (thechoosentoppings[a]==="onions"){
   additiontoppings +=500
-  console.log(additiontoppings)
+  console.log(additiontoppings)will be delivered at
 }
 else if (thechoosentoppings[a]==="sausage"){
   additiontoppings +=1000
@@ -89,20 +66,17 @@ else if (thechoosentoppings[a]==="green-peppers"){
 
 }
 console.log(additiontoppings)
-return additiontoppings;
-}
 var inputtedquantityofpizza
 var inputteddeliveryofpizza = $('#delivery').val()
-toppings(thechoosentoppings)
+
 if($('#hide3').val()){
       inputtedquantityofpizza=$('#hide3').val();
         console.log(inputtedquantityofpizza)
     }
-
-    
-      if($('#hide3').val()){
+  if($('#hide3').val()){
         console.log()
       }
+    var newOrder;
     var newOrder = new Order(inputtedsizeofpizza,inputtedcrustofpizza,additiontoppings,inputtedquantityofpizza,inputteddeliveryofpizza);
     console.log(newOrder)
     console.log($('#delivery').val())
@@ -112,9 +86,14 @@ if($('#hide3').val()){
  
     $(".pizza-toppings").text(newOrder.toppingsofpizza + 'frw');
     $(".pizza-quantity").text(newOrder.quantityofpizza);
-   if($('#delivery').val() === "2000"){
+
+   if($('#delivery').val() === "2000frw"){
      $(".pizza-delivery").text("The delivery is cost" +$('#delivery').val());
-    }
+  
+    var seelocation=prompt("where is your lovation:");
+
+    alert("our order will be delivered to " + seelocation);
+  }
     else{
        $(".pizza-delivery").text("you haven't requested any delivery option");
       }
