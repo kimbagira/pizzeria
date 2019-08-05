58754f34c6e325eb486fd7c4250564a53a78b5dd
kimbagira Sylvie <sylviekimbagira@gmail.com>
2019-08-05 16:00:48 +0200

    added indentation

--- js/scripts.js
+++ js/scripts.js
@@ -19,70 +19,55 @@ $('form#pizza').submit(function(event){
   event.preventDefault();
 
 var inputtedsizeofpizza=$("#hide").val();
-  // console.log(inputtedsizeofpizza)
   var inputtedcrustofpizza=$("#hide1").val();
-  // console.log(inputtedcrustofpizza)
 
   var thechoosentoppings=[];
   $('input[type=checkbox]:checked').each(function () {
     thechoosentoppings.push($(this).val());
 });
 
-  // console.log(thechoosentoppings);
   var additiontoppings = 0;
 for(var a=0; a<thechoosentoppings.length; a++){
   if(thechoosentoppings[a]==="pepperont"){
     additiontoppings += 500
-    // console.log(additiontoppings)
   }
  else if (thechoosentoppings[a]==="mushrooms"){
    additiontoppings +=600
-  //  console.log(additiontoppings)
  }
  else if (thechoosentoppings[a]==="onions"){
   additiontoppings +=500
-  // console.log(additiontoppings)
 }
 else if (thechoosentoppings[a]==="sausage"){
   additiontoppings +=1000
-  // console.log(additiontoppings)
 }
 else if (thechoosentoppings[a]==="bacons"){
   additiontoppings +=600
-  // console.log(additiontoppings)
 }
 else if (thechoosentoppings[a]==="extra-cheese"){
   additiontoppings +=1000
-  // console.log(additiontoppings)
 }
 else if (thechoosentoppings[a]==="black-olives"){
   additiontoppings +=800
-  // console.log(additiontoppings)
 }
 else if (thechoosentoppings[a]==="green-peppers"){
   additiontoppings +=400
-  // console.log(additiontoppings)
 }else{
   additiontoppings +=300
-  // console.log(additiontoppings)
 }
 
 }
-// console.log(additiontoppings)
 var inputtedquantityofpizza
 var inputteddeliveryofpizza = $('#delivery').val()
 
 if($('#hide3').val()){
       inputtedquantityofpizza=$('#hide3').val();
-        // console.log(inputtedquantityofpizza)
     }
   if($('#hide3').val()){
         console.log()
       }
     var newOrder;
     var newOrder = new Order(inputtedsizeofpizza,inputtedcrustofpizza,additiontoppings,inputtedquantityofpizza,inputteddeliveryofpizza);
-    // console.log(newOrder.allInformation())
-    // console.log($('#delivery').val())
+    
     $("#get-pizza").show();
     $(".pizza-size").text(newOrder.sizeofpizza);
     $(".pizza-crust").text(newOrder.crustofpizza);
